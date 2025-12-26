c6f6ba9e967a754aeec5972471d24499f805a55b
adithyan <adithyansmkd007@gmail.com>
2025-12-26 22:06:51 +0530

    fourth

--- chattingarena/asgi.py
+++ chattingarena/asgi.py
@@ -1,13 +1,19 @@
 import os
 from django.core.asgi import get_asgi_application
+
+# Set Django settings module FIRST
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chattingarena.settings')
+
+# Initialize Django BEFORE importing anything else
+django_asgi_app = get_asgi_application()
+
+# NOW import channels and custom middleware (after Django is initialized)
 from channels.routing import ProtocolTypeRouter, URLRouter
 from chat.middleware import TokenAuthMiddlewareStack
 import chat.routing
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chattingarena.settings')
-
 application = ProtocolTypeRouter({
-    "http": get_asgi_application(),
+    "http": django_asgi_app,
     "websocket": TokenAuthMiddlewareStack(
         URLRouter(
             chat.routing.websocket_urlpatterns
