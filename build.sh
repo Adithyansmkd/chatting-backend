25244525f289535a60c92d4a1527d82049f22758
adithyan <adithyansmkd007@gmail.com>
2025-12-27 17:09:08 +0530

    Fix web

--- build.sh
+++ build.sh
@@ -2,11 +2,11 @@
 # Exit on error
 set -o errexit
 
-# Install dependencies
+# Install dependencies (ensure they are installed before any django command)
 pip install -r requirements.txt
+
+# Run migrations first (safest)
+python manage.py migrate
 
 # Collect static files
 python manage.py collectstatic --no-input
-
-# Run database migrations (Automated)
-python manage.py migrate
